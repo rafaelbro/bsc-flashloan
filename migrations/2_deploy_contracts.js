dd40dd093e2859dda4d6b6ebec4b10777a74799f
Rafael <rafael.heck@outlook.com>
2021-06-02 17:48:26 -0300

    added gitignore

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,19 @@
 const Arbitrage = artifacts.require("Arbitrage.sol");
 
+
+module.exports = function (deployer) {
+  deployer.deploy(
+    Arbitrage,
+    '0xBCfCcbde45cE874adCB698cC183deBcF17952812', //PancakeSwap factory
+    '0xF3feD705Db9974C6eEAa5cbE79FBe9BEb7D6F459', //BakerySwap router
+  );
+
+/*
 module.exports = function (deployer) {
   deployer.deploy(
     Arbitrage,
     '0xBCfCcbde45cE874adCB698cC183deBcF17952812', //PancakeSwap factory
     'couldlt find their address :( if you try to trade with a liquidity pool on their website, metamask should show you the address of their router', //BakerySwap router
   );
+*/
 };
